c81cad41ca095a2aab962f9986cd711f490bd871
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-04-24 17:42:10 -0400

    Initial Commit

---
+++ Shared/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  Shared
+//
+//  Created by Andrew Chen on 2021-04-24.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
