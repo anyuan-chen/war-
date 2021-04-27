c0a75b7ee0e1d433828342d860b1d22f9d41af85
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-04-27 17:30:54 -0400

    added state variables for dynamic operations

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerCard = "card5"
+    @State var cpuCard = "card9"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        Image("logo")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
+        ZStack{
+            Image("background").ignoresSafeArea();
+            
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    playerCard = "card2"
+                    playerScore+=1
+                }, label: {
+                    Image("dealbutton")
+                })
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                        Text(String(playerScore))
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                        Text(String(cpuScore))
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
     }
+
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
